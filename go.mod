b75e5f3fd8cf8bd2a4dde029ffc59d53c2e72f93
sangock <senseoki@gmail.com>
2019-12-09 18:09:57 +0900

    add iris-zap-test

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/senseoki/ws-go-practice
 
 go 1.13
+
+require (
+	github.com/astaxie/beego v1.12.0 // indirect
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/kataras/iris v11.1.1+incompatible
+	github.com/kataras/iris/v12 v12.0.1
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	go.uber.org/atomic v1.5.1 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
+	go.uber.org/zap v1.13.0
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
+	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	golang.org/x/net v0.0.0-20191207000613-e7e4b65ae663 // indirect
+	golang.org/x/sys v0.0.0-20191206220618-eeba5f6aabab // indirect
+	golang.org/x/tools v0.0.0-20191206204035-259af5ff87bd // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
---
+++ iris-zap-test/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kataras/iris/v12"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func main() {
+	app := iris.New()
+
+	logger := zapConfig()
+
+	app.Get("/ping", func(ctx iris.Context) {
+		for i := 0; i < 1000000; i++ {
+			logger.Error("This is an ERROR message")
+		}
+		ctx.WriteString("pong")
+	})
+
+	app.Get("/test01", func(ctx iris.Context) {
+		ctx.WriteString("test01")
+	})
+
+	logger.Info("Logger Test Info",
+		zap.String("url", "http://test.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+	logger.Error("This is an ERROR message")
+
+	//logger.DPanic("This is a DPANIC message")
+
+	app.Run(
+		iris.Addr("0.0.0.0:9100"),
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+		iris.WithoutInterruptHandler,
+	)
+
+}
+
+func zapConfig() *zap.Logger {
+	now := time.Now()
+	hook := lumberjack.Logger{
+		Filename:   fmt.Sprintf("logs/%04d%02d%02d%02d%02d%02d.log", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()),
+		MaxSize:    1,
+		MaxBackups: 30,
+		MaxAge:     7,
+		Compress:   false,
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.FullCallerEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	atomicLevel := zap.NewAtomicLevel()
+	atomicLevel.SetLevel(zap.InfoLevel)
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)),
+		atomicLevel,
+	)
+
+	caller := zap.AddCaller()
+	development := zap.Development()
+	filed := zap.Fields(zap.String("serviceName", "serviceName"))
+	logger := zap.New(core, caller, development, filed)
+
+	return logger
+}
+
+// func zapConfig() *zap.Logger {
+
+// 	// w := zapcore.AddSync(&lumberjack.Logger{
+// 	// 	Filename: "logdata/foo.log",
+// 	// 	MaxSize:  1, // megabytes
+// 	// 	//MaxBackups: 3,
+// 	// 	MaxAge: 30, // days
+// 	// })
+// 	// core := zapcore.NewCore(
+// 	// 	zapcore.NewJSONEncoder(
+// 	// 		// zap.NewProductionEncoderConfig(),
+// 	// 		zap.NewDevelopmentEncoderConfig(),
+// 	// 	),
+// 	// 	w,
+// 	// 	zap.InfoLevel,
+// 	// )
+
+// 	// logger := zap.New(core)
+
+// 	now := time.Now()
+// 	hook := &lumberjack.Logger{
+// 		Filename: fmt.Sprintf("log/%04d%02d%02d%02d%02d%02d.log", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()), //filePath
+// 		MaxSize:  1,                                                                                                                           // megabytes
+// 		//MaxBackups: 10000,
+// 		MaxAge:   30,    //days
+// 		Compress: false, // disabled by default
+// 	}
+// 	defer hook.Close()
+
+// 	enConfig := zap.NewProductionEncoderConfig()
+// 	enConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+// 	level := zap.InfoLevel
+// 	w := zapcore.AddSync(hook)
+// 	core := zapcore.NewCore(
+// 		zapcore.NewConsoleEncoder(enConfig),
+// 		w,
+// 		level,
+// 	)
+
+// 	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+// 	return logger
+// }
